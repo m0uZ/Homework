9cf57c278d4b3a3b3e815ff47d5705ea3570f5ea
m0uZ <m.fraltsov@gmail.com>
2022-08-19 00:46:37 +0300

    Решение четвертой задачи

---
+++ task004/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите первое число и нажмите клавишу Enter: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число и нажмите клавишу Enter: ");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число и нажмите клавишу Enter: ");
+
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine("max = " + max);
