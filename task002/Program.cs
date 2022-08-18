8e349dec9a1dd24f99d941b1be71bf088c6fbe30
m0uZ <m.fraltsov@gmail.com>
2022-08-19 00:19:50 +0300

    Решение второй задачи

---
+++ task002/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число и нажмите клавишу Enter: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число и нажмите клавишу Enter: ");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("Максимальное число " + a);
+}
+else
+{
+    Console.WriteLine("Максимальное число " + b);
+}
