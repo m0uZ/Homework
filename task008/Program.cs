e38e3e215984fddd8ca2e597692d8f9f26fa69d5
m0uZ <m.fraltsov@gmail.com>
2022-08-19 03:42:29 +0300

    Решение восьмой задачи

---
+++ task008/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число и нажмите клавишу Enter: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+int i = 2;
+
+while (i <= a)
+{
+    if (i % 2 == 0)
+    Console.WriteLine(i);
+    i++;
+}
